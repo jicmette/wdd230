935871590a74c57e4d3c3e21b62a4f14c021cdd1
jicmette <jicmettesc@gmail.com>
2023-05-26 13:10:45 -0600

    Images not used in this assignment deleted

--- lesson06/scripts/windchill.js
+++ lesson06/scripts/windchill.js
@@ -3,9 +3,9 @@ let speed = parseFloat(document.querySelector("#speed").textContent);
 let formula = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
 
 if (temp <= 50 && speed > 3){
-    F = formula.toFixed(1);
+    windChill = formula.toFixed(1);
 } else {
-    F = "N/A";
+    windChill = "N/A";
 }
 
-document.querySelector("#windchill").innerHTML = F;
+document.querySelector("#windchill").innerHTML = windChill;
