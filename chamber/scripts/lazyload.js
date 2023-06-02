c2b3c400ee8cc780563fc35420442768a8972f71
jicmette <jicmettesc@gmail.com>
2023-06-01 19:23:11 -0600

    Pictures for Discover Page added, with structure and layout for responsive views. Lazy loading added.

---
+++ chamber/scripts/lazyload.js
@@ -0,0 +1,32 @@
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+const imgOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px 50px 0px",
+};
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        loadImages(entry.target);
+        observer.unobserve(entry.target);
+      }
+    });
+  }, imgOptions);
+    imagesToLoad.forEach((image) => {
+    observer.observe(image);
+  });
+} else {
+    imagesToLoad.forEach((image) => {
+        loadImages(image);
+});
+
+}
