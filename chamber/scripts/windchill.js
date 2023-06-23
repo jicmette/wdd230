1d7256ef2226b21c390102f35de1c934ff7d8b28
jicmette <jicmettesc@gmail.com>
2023-06-23 13:48:09 -0600

    First test Weather API Data in home page.

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,5 +1,5 @@
-let temp = parseFloat(document.querySelector("#temp").textContent);
-let speed = parseFloat(document.querySelector("#speed").textContent);
+let temp = document.querySelector("#temp");
+let speed = document.querySelector("#speed");
 let formula = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16;
 
 if (temp <= 50 && speed > 3){
