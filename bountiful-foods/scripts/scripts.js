0b9671d33d80b4eb6583327bd277adeb9deeec7b
jicmette <jicmettesc@gmail.com>
2023-07-14 18:56:27 -0600

    Carousel Fresh Page added first test for touch dragging

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -4,26 +4,57 @@ document.querySelector(".currentYear").innerText = year;
 
 //Carousel Fresh Page
 const carousel = document.querySelector(".carousel");
+firstImg = carousel.querySelectorAll("img")[0];
+const arrowIcons = document.querySelectorAll(".wrapper i");
 
-let isDragStart = false, prevPageX, prevScrollLeft;
+let isDragStart = false, prevPageX, prevScrollLeft, positionDiff;
+
+const showHideIcons = () => {
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+
+}
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; 
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60);
+    });
+});
+
+const autoSlide = () => {
+    console.log(positionDiff);
+}
 
 const dragStart = (e) => {
     isDragStart = true;
-    prevPageX = e.pageX;
+    prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 
 const dragging = (e) => {
     if(!isDragStart) return;
     e.preventDefault();
-    let positionDiff = e.pageX - prevPageX;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX)- prevPageX;
     carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
 }
 
 const dragStop = () => {
     isDragStart = false;
+    carousel.classList.remove("dragging");
+    autoSlide();
 }
 
 carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
 carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
