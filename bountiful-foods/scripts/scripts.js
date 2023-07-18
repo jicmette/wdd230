4243e0f69e87a2e2ef1ae9c2a5ee1b31d4f11418
jicmette <jicmettesc@gmail.com>
2023-07-18 16:53:20 -0600

    Some fonts resized for a better display in mobile view

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -71,18 +71,56 @@ carousel.addEventListener("mouseleave", dragStop);
 carousel.addEventListener("touchend", dragStop);
 
 //JSON for select fruits in Fresh Page
-const fruits = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+// const fruits = "https://brotherblazzard.github.io/canvas-content/fruit.json";
 
-async function getFruitsData() {
-  const response = await fetch(fruits);
-  const data = await response.json();
-  console.table(data);
-}
+// async function getFruitsData() {
+//   const response = await fetch(fruits);
+//   const data = await response.json();
+//   console.table(data);
+// }
 
-for (let name in fruits) {
-    html += "<option value=" + name + ">" +fruits[name] + "</option>"
-}
+// for (let name in fruits) {
+//     html += "<option value=" + name + ">" +fruits[name] + "</option>"
+// }
 
-document.getElementById("fruit").innerHTML = f
+// document.getElementById("fruit").innerHTML = f
 
-getFruitsData();
+// getFruitsData();
+
+// let dropdown = document.getElementById("#fruits");
+// dropdown.length = 0;
+
+// let defaultOption = document.createElement("option");
+// defaultOption.text = "Choose a Fruit";
+
+// dropdown.add(defaultOption);
+// dropdown.selectedIndex = 0;
+
+// const url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+
+// fetch(url)
+//   .then(function (response) {
+//     if (response.status !== 200) {
+//       console.warn(
+//         "Looks like there was a problem. Status Code: " + response.status
+//       );
+//       return;
+//     }
+
+//     // Examine the text in the response
+//     response.json().then(function (data) {
+//       let option;
+
+//       for (let i = 0; i < data.length; i++) {
+//         option = document.createElement("option");
+//         option.text = data[i].name;
+//         dropdown.add(option);
+//       }
+//     });
+//   })
+
+//   .catch(function (err) {
+//     console.error("Fetch Error -", err);
+//   });
+
+//   fetch(url);
