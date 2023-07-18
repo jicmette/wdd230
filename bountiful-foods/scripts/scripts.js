a5f01e80af9c7f8e7d4754f697b5ca453fd54e25
jicmette <jicmettesc@gmail.com>
2023-07-18 16:29:19 -0600

    Test for responsive design Fresh Page

--- bountiful-foods/scripts/scripts.js
+++ bountiful-foods/scripts/scripts.js
@@ -2,52 +2,63 @@
 const year = new Date().getFullYear();
 document.querySelector(".currentYear").innerText = year;
 
+//Hamburguer Nav
+const hamburger = document.querySelector("#hamburger");
+const navigation = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+  navigation.classList.toggle("responsive");
+});
+
 //Carousel Fresh Page
 const carousel = document.querySelector(".carousel");
 firstImg = carousel.querySelectorAll("img")[0];
 const arrowIcons = document.querySelectorAll(".wrapper i");
 
-let isDragStart = false, prevPageX, prevScrollLeft, positionDiff;
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft,
+  positionDiff;
 
 const showHideIcons = () => {
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
-    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
-    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+  let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
+  arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+  arrowIcons[1].style.display =
+    carousel.scrollLeft == scrollWidth ? "none" : "block";
+};
 
-}
-
-arrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; 
-        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60);
-    });
+arrowIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let firstImgWidth = firstImg.clientWidth + 14;
+    carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+    setTimeout(() => showHideIcons(), 60);
+  });
 });
 
 const autoSlide = () => {
-    console.log(positionDiff);
-}
+  console.log(positionDiff);
+};
 
 const dragStart = (e) => {
-    isDragStart = true;
-    prevPageX = e.pageX || e.touches[0].pageX;
-    prevScrollLeft = carousel.scrollLeft;
-}
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
 
 const dragging = (e) => {
-    if(!isDragStart) return;
-    e.preventDefault();
-    carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX)- prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
-    showHideIcons();
-}
+  if (!isDragStart) return;
+  e.preventDefault();
+  carousel.classList.add("dragging");
+  positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+  showHideIcons();
+};
 
 const dragStop = () => {
-    isDragStart = false;
-    carousel.classList.remove("dragging");
-    autoSlide();
-}
+  isDragStart = false;
+  carousel.classList.remove("dragging");
+  autoSlide();
+};
 
 carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("touchstart", dragStart);
@@ -58,3 +69,20 @@ carousel.addEventListener("touchmove", dragging);
 carousel.addEventListener("mouseup", dragStop);
 carousel.addEventListener("mouseleave", dragStop);
 carousel.addEventListener("touchend", dragStop);
+
+//JSON for select fruits in Fresh Page
+const fruits = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+
+async function getFruitsData() {
+  const response = await fetch(fruits);
+  const data = await response.json();
+  console.table(data);
+}
+
+for (let name in fruits) {
+    html += "<option value=" + name + ">" +fruits[name] + "</option>"
+}
+
+document.getElementById("fruit").innerHTML = f
+
+getFruitsData();
